081866545a1700e909e424e385865b0270bffb6e
Zakhary <zakharyanton@gmail.com>
2021-09-17 15:47:32 +0300

    6: Optimizing The Render Pipeline In Swift And Metal

--- Game Engine/Game Engine/Display/GameView.swift
+++ Game Engine/Game Engine/Display/GameView.swift
@@ -9,23 +9,20 @@ import MetalKit
 
 class GameView: MTKView {
     
-    var commandQueue : MTLCommandQueue!
-    var renderPipelineState : MTLRenderPipelineState!
-    
     var vertices : [Vertex]!
-    
     var vertexBuffer: MTLBuffer!
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
+        
         self.device = MTLCreateSystemDefaultDevice()
         
+        Engine.Ignite(device: device!)
+        
         //color that wil be in the back, when we redraw view
-        self.clearColor = MTLClearColor(red: 0.43, green: 0.85, blue: 0.35, alpha: 1.0)
+        self.clearColor = Preferences.ClearColor
         
-        self.colorPixelFormat = .bgra8Unorm
-        
-        self.commandQueue = device?.makeCommandQueue()
+        self.colorPixelFormat = Preferences.MainPixelFormat
         
         //состояние конвейера рендеринга : Render pipeline state
         /*
@@ -35,8 +32,6 @@ class GameView: MTKView {
          - vertex function
          - fragment function
          */
-        
-        createRenderPipelineState()
         
         createVertices()
         
@@ -57,42 +52,10 @@ class GameView: MTKView {
         vertexBuffer = device?.makeBuffer(bytes: vertices, length: Vertex.stride(vertices.count), options: [])
     }
     
-    func createRenderPipelineState() {
-        let library = device?.makeDefaultLibrary()
-        let vertexFunction = library?.makeFunction(name: "basic_vertex_shader")
-        let fragmentFunction = library?.makeFunction(name: "basic_fragment_shader")
-        
-        let vertexDescriptor = MTLVertexDescriptor()
-        
-        //Position
-        vertexDescriptor.attributes[0].format = .float3
-        vertexDescriptor.attributes[0].bufferIndex = 0
-        vertexDescriptor.attributes[0].offset = 0
-        
-        //Color
-        vertexDescriptor.attributes[1].format = .float4
-        vertexDescriptor.attributes[1].bufferIndex = 0
-        vertexDescriptor.attributes[1].offset = SIMD3<Float>.stride()
-        
-        vertexDescriptor.layouts[0].stride = Vertex.stride()
-        
-        let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
-        renderPipelineDescriptor.colorAttachments[0].pixelFormat = .bgra8Unorm
-        renderPipelineDescriptor.vertexFunction = vertexFunction
-        renderPipelineDescriptor.fragmentFunction = fragmentFunction
-        renderPipelineDescriptor.vertexDescriptor = vertexDescriptor
-        
-        do {
-            renderPipelineState = try device?.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
-        } catch let error as NSError {
-            print(error)
-        }
-    }
-    
     override func draw(_ dirtyRect: NSRect) {
         guard let drawble = self.currentDrawable, let renderPassDescriptor = self.currentRenderPassDescriptor else { return }
         //buffer of our tasks(bf1 computing texture, bf2 render texture)
-        let commandBuffer = commandQueue.makeCommandBuffer()
+        let commandBuffer = Engine.CommandQueue.makeCommandBuffer()
         /*Command Encoder 4 types:
          - render Command Encoder (render graphics)
          - compute Command Encoder(computation tasks)
@@ -100,8 +63,8 @@ class GameView: MTKView {
          - parallel Command Encoder (multiple graphic rendaring task)
          */
         let renderCommandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: renderPassDescriptor)
-        renderCommandEncoder?.setRenderPipelineState(renderPipelineState)
         
+        renderCommandEncoder?.setRenderPipelineState(RenderPipelineStateLibrary.PipelineState(.Basic))
         renderCommandEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
         renderCommandEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: vertices.count)
         //send info to rendercommandcoder
