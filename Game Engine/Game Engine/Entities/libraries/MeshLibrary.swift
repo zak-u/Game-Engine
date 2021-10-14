3c98f4c1a6adc1428c79c46f2b51232ca5b95099
Zakhary <zakharyanton@gmail.com>
2021-10-14 18:08:51 +0300

    17: + some fix

--- Game Engine/Game Engine/Entities/libraries/MeshLibrary.swift
+++ Game Engine/Game Engine/Entities/libraries/MeshLibrary.swift
@@ -23,18 +23,17 @@ class MeshLibrary: Library<MeshTypes, Mesh> {
 }
 
 protocol Mesh {
-    var vertexBuffer: MTLBuffer! { get }
     var vertexCount: Int! { get }
     func setInstanceCount(_ count: Int)
     func drawPrimitives(_ renderCommandEncoder: MTLRenderCommandEncoder)
 }
 
 class CustomMesh: Mesh {
-    var vertices: [Vertex]!
-    var vertexBuffer: MTLBuffer!
-    var instanceCount: Int = 1
+    private var _vertices: [Vertex] = []
+    private var _vertexBuffer: MTLBuffer!
+    private var _instanceCount: Int = 1
     var vertexCount: Int! {
-        return vertices.count
+        return _vertices.count
     }
     
     init() {
@@ -45,97 +44,100 @@ class CustomMesh: Mesh {
     func createVertices(){ }
     
     func createBuffers(){
-        vertexBuffer = Engine.Device.makeBuffer(bytes: vertices, length: Vertex.stride(vertices.count), options: [])
+        _vertexBuffer = Engine.Device.makeBuffer(bytes: _vertices,
+                                                length: Vertex.stride(vertexCount),
+                                                options: [])
     }
+    
+    func addVertex(position:  SIMD3<Float>,
+                   color:  SIMD4<Float> =  SIMD4<Float>(1,0,1,1)) {
+        _vertices.append(Vertex(position: position, color: color))
+    }
+    
     func setInstanceCount(_ count: Int) {
-            self.instanceCount = count
-        }
-        
+        self._instanceCount = count
+    }
+    
     func drawPrimitives(_ renderCommandEncoder: MTLRenderCommandEncoder) {
-        renderCommandEncoder.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+        renderCommandEncoder.setVertexBuffer(_vertexBuffer, offset: 0,
+                                             index: 0)
         
         renderCommandEncoder.drawPrimitives(type: .triangle,
                                             vertexStart: 0,
                                             vertexCount: vertexCount,
-                                            instanceCount: instanceCount)
+                                            instanceCount: _instanceCount)
     }
 }
 
 class Triangle_CustomMesh: CustomMesh {
     override func createVertices() {
-        vertices = [
-            Vertex(position: SIMD3<Float>(0,1,0), color: SIMD4<Float>(1,0,0,1)),
-            Vertex(position: SIMD3<Float>(-1,-1,0), color: SIMD4<Float>(0,1,0,1)),
-            Vertex(position: SIMD3<Float>(1,-1,0), color: SIMD4<Float>(0,0,1,1)),
-        ]
+        addVertex(position:  SIMD3<Float>( 0, 1,0), color:  SIMD4<Float>(1,0,0,1))
+        addVertex(position:  SIMD3<Float>(-1,-1,0), color:  SIMD4<Float>(0,1,0,1))
+        addVertex(position:  SIMD3<Float>( 1,-1,0), color:  SIMD4<Float>(0,0,1,1))
     }
 }
 
 class Quad_CustomMesh: CustomMesh {
     override func createVertices() {
-        vertices = [
-            Vertex(position: SIMD3<Float>( 1, 1,0), color: SIMD4<Float>(1,0,0,1)), //Top Right
-            Vertex(position: SIMD3<Float>(-1, 1,0), color: SIMD4<Float>(0,1,0,1)), //Top Left
-            Vertex(position: SIMD3<Float>(-1,-1,0), color: SIMD4<Float>(0,0,1,1)),  //Bottom Left
-            
-            Vertex(position: SIMD3<Float>( 1, 1,0), color: SIMD4<Float>(1,0,0,1)), //Top Right
-            Vertex(position: SIMD3<Float>(-1,-1,0), color: SIMD4<Float>(0,0,1,1)), //Bottom Left
-            Vertex(position: SIMD3<Float>( 1,-1,0), color: SIMD4<Float>(1,0,1,1))  //Bottom Right
-        ]
+        addVertex(position:  SIMD3<Float>( 1, 1,0), color:  SIMD4<Float>(1,0,0,1)) //Top Right
+        addVertex(position:  SIMD3<Float>(-1, 1,0), color:  SIMD4<Float>(0,1,0,1)) //Top Left
+        addVertex(position:  SIMD3<Float>(-1,-1,0), color:  SIMD4<Float>(0,0,1,1)) //Bottom Left
+        
+        addVertex(position:  SIMD3<Float>( 1, 1,0), color:  SIMD4<Float>(1,0,0,1)) //Top Right
+        addVertex(position:  SIMD3<Float>(-1,-1,0), color:  SIMD4<Float>(0,0,1,1)) //Bottom Left
+        addVertex(position:  SIMD3<Float>( 1,-1,0), color:  SIMD4<Float>(1,0,1,1)) //Bottom Right
     }
 }
 
 class Cube_CustomMesh: CustomMesh {
     override func createVertices() {
-        vertices = [
-            //Left
-            Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), color: SIMD4<Float>(1.0, 0.5, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0,-1.0, 1.0), color: SIMD4<Float>(0.0, 1.0, 0.5, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), color: SIMD4<Float>(0.0, 0.5, 1.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), color: SIMD4<Float>(1.0, 1.0, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), color: SIMD4<Float>(0.0, 1.0, 1.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0, 1.0,-1.0), color: SIMD4<Float>(1.0, 0.0, 1.0, 1.0)),
-            
-            //RIGHT
-            Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), color: SIMD4<Float>(1.0, 0.0, 0.5, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0,-1.0,-1.0), color: SIMD4<Float>(0.0, 1.0, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0, 1.0,-1.0), color: SIMD4<Float>(0.0, 0.5, 1.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0,-1.0,-1.0), color: SIMD4<Float>(1.0, 1.0, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), color: SIMD4<Float>(0.0, 1.0, 1.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), color: SIMD4<Float>(1.0, 0.5, 1.0, 1.0)),
-            
-            //TOP
-            Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), color: SIMD4<Float>(1.0, 0.0, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0, 1.0,-1.0), color: SIMD4<Float>(0.0, 1.0, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0, 1.0,-1.0), color: SIMD4<Float>(0.0, 0.0, 1.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), color: SIMD4<Float>(1.0, 1.0, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0, 1.0,-1.0), color: SIMD4<Float>(0.5, 1.0, 1.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), color: SIMD4<Float>(1.0, 0.0, 1.0, 1.0)),
-            
-            //BOTTOM
-            Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), color: SIMD4<Float>(1.0, 0.5, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), color: SIMD4<Float>(0.5, 1.0, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0,-1.0,-1.0), color: SIMD4<Float>(0.0, 0.0, 1.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), color: SIMD4<Float>(1.0, 1.0, 0.5, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0,-1.0, 1.0), color: SIMD4<Float>(0.0, 1.0, 1.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), color: SIMD4<Float>(1.0, 0.5, 1.0, 1.0)),
-            
-            //BACK
-            Vertex(position: SIMD3<Float>( 1.0, 1.0,-1.0), color: SIMD4<Float>(1.0, 0.5, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), color: SIMD4<Float>(0.5, 1.0, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0, 1.0,-1.0), color: SIMD4<Float>(0.0, 0.0, 1.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0, 1.0,-1.0), color: SIMD4<Float>(1.0, 1.0, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0,-1.0,-1.0), color: SIMD4<Float>(0.0, 1.0, 1.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0,-1.0,-1.0), color: SIMD4<Float>(1.0, 0.5, 1.0, 1.0)),
-            
-            //FRONT
-            Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), color: SIMD4<Float>(1.0, 0.5, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0,-1.0, 1.0), color: SIMD4<Float>(0.0, 1.0, 0.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), color: SIMD4<Float>(0.5, 0.0, 1.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0, 1.0, 1.0), color: SIMD4<Float>(1.0, 1.0, 0.5, 1.0)),
-            Vertex(position: SIMD3<Float>(-1.0, 1.0, 1.0), color: SIMD4<Float>(0.0, 1.0, 1.0, 1.0)),
-            Vertex(position: SIMD3<Float>( 1.0,-1.0, 1.0), color: SIMD4<Float>(1.0, 0.0, 1.0, 1.0))
-        ]
+        //Left
+        addVertex(position:  SIMD3<Float>(-1.0,-1.0,-1.0), color:  SIMD4<Float>(1.0, 0.5, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0,-1.0, 1.0), color:  SIMD4<Float>(0.0, 1.0, 0.5, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0, 1.0, 1.0), color:  SIMD4<Float>(0.0, 0.5, 1.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0,-1.0,-1.0), color:  SIMD4<Float>(1.0, 1.0, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0, 1.0, 1.0), color:  SIMD4<Float>(0.0, 1.0, 1.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0, 1.0,-1.0), color:  SIMD4<Float>(1.0, 0.0, 1.0, 1.0))
+        
+        //RIGHT
+        addVertex(position:  SIMD3<Float>( 1.0, 1.0, 1.0), color:  SIMD4<Float>(1.0, 0.0, 0.5, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0,-1.0,-1.0), color:  SIMD4<Float>(0.0, 1.0, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0, 1.0,-1.0), color:  SIMD4<Float>(0.0, 0.5, 1.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0,-1.0,-1.0), color:  SIMD4<Float>(1.0, 1.0, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0, 1.0, 1.0), color:  SIMD4<Float>(0.0, 1.0, 1.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0,-1.0, 1.0), color:  SIMD4<Float>(1.0, 0.5, 1.0, 1.0))
+        
+        //TOP
+        addVertex(position:  SIMD3<Float>( 1.0, 1.0, 1.0), color:  SIMD4<Float>(1.0, 0.0, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0, 1.0,-1.0), color:  SIMD4<Float>(0.0, 1.0, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0, 1.0,-1.0), color:  SIMD4<Float>(0.0, 0.0, 1.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0, 1.0, 1.0), color:  SIMD4<Float>(1.0, 1.0, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0, 1.0,-1.0), color:  SIMD4<Float>(0.5, 1.0, 1.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0, 1.0, 1.0), color:  SIMD4<Float>(1.0, 0.0, 1.0, 1.0))
+        
+        //BOTTOM
+        addVertex(position:  SIMD3<Float>( 1.0,-1.0, 1.0), color:  SIMD4<Float>(1.0, 0.5, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0,-1.0,-1.0), color:  SIMD4<Float>(0.5, 1.0, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0,-1.0,-1.0), color:  SIMD4<Float>(0.0, 0.0, 1.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0,-1.0, 1.0), color:  SIMD4<Float>(1.0, 1.0, 0.5, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0,-1.0, 1.0), color:  SIMD4<Float>(0.0, 1.0, 1.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0,-1.0,-1.0), color:  SIMD4<Float>(1.0, 0.5, 1.0, 1.0))
+        
+        //BACK
+        addVertex(position:  SIMD3<Float>( 1.0, 1.0,-1.0), color:  SIMD4<Float>(1.0, 0.5, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0,-1.0,-1.0), color:  SIMD4<Float>(0.5, 1.0, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0, 1.0,-1.0), color:  SIMD4<Float>(0.0, 0.0, 1.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0, 1.0,-1.0), color:  SIMD4<Float>(1.0, 1.0, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0,-1.0,-1.0), color:  SIMD4<Float>(0.0, 1.0, 1.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0,-1.0,-1.0), color:  SIMD4<Float>(1.0, 0.5, 1.0, 1.0))
+        
+        //FRONT
+        addVertex(position:  SIMD3<Float>(-1.0, 1.0, 1.0), color:  SIMD4<Float>(1.0, 0.5, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0,-1.0, 1.0), color:  SIMD4<Float>(0.0, 1.0, 0.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0,-1.0, 1.0), color:  SIMD4<Float>(0.5, 0.0, 1.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0, 1.0, 1.0), color:  SIMD4<Float>(1.0, 1.0, 0.5, 1.0))
+        addVertex(position:  SIMD3<Float>(-1.0, 1.0, 1.0), color:  SIMD4<Float>(0.0, 1.0, 1.0, 1.0))
+        addVertex(position:  SIMD3<Float>( 1.0,-1.0, 1.0), color:  SIMD4<Float>(1.0, 0.0, 1.0, 1.0))
     }
 
 }
